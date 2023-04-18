4682c36ea6068ae5430b64161d5f4370ee60381d
MantasPetrosiusI <mantas.petrosius@outlook.com>
2023-04-18 11:14:56 +0100

    z

--- src/redux/interfaces/index.ts
+++ src/redux/interfaces/index.ts
@@ -1,75 +1,74 @@
 import { UserInfo } from "os";
 
 export interface RootState {
-    whatsApp: whatsAppState;
+  whatsApp: whatsAppState;
 }
 
 export interface whatsAppState {
-    userInfo: {
-        _id: string
-        name: string
-        email: string
-        avatar?: string
-    },
-    chats: {
-        active: string // the _id of one of the chats among store.chats.list	
-        list: Chat[]
-    }
+  userInfo: {
+    _id: string;
+    name: string;
+    email: string;
+    avatar?: string;
+  };
+  chats: {
+    active: string; // the _id of one of the chats among store.chats.list
+    list: Chat[];
+  };
 }
 
-interface Chat {
-    _id: string
-	members: User[]
-	messages: Message[]
+export interface Chat {
+  _id: string;
+  members: User[];
+  messages: Message[];
 }
 
 export interface Message {
-    _id: string
-	sender: User
-	content: {
-		text?: string
-		media?: string
-	}
-	timestamp: number
+  _id: string;
+  sender: User;
+  content: {
+    text?: string;
+    media?: string;
+  };
+  timestamp: number;
 }
 
 export interface User {
-    _id: string
-    name: string
-    email: string
-    avatar?: string
+  _id: string;
+  name: string;
+  email: string;
+  avatar?: string;
 }
 
-export interface setUserInfo{
-    type:"SET_USER_INFO";
-    payload: User;
+export interface setUserInfo {
+  type: "SET_USER_INFO";
+  payload: User;
 }
 
 export interface setChats {
-    type:"SET_CHATS";
-    payload: { 
-        chats: Chat[] 
-    }
+  type: "SET_CHATS";
+  payload: {
+    chats: Chat[];
+  };
 }
 export interface setActiveChat {
-    type:"SET_ACTIVE_CHAT";
-    payload: { 
-        chatId: string 
-    }
+  type: "SET_ACTIVE_CHAT";
+  payload: {
+    chatId: string;
+  };
 }
 export interface setHistory {
-    type:"SET_HISTORY";
-    payload: { 
-        chatId: string, 
-        history: Message[] 
-    }
+  type: "SET_HISTORY";
+  payload: {
+    chatId: string;
+    history: Message[];
+  };
 }
 
 export interface newMessage {
-    type:"NEW_MESSAGE";
-    payload: {
-        chatId: string, 
-        message: Message
-    };
+  type: "NEW_MESSAGE";
+  payload: {
+    chatId: string;
+    message: Message;
+  };
 }
-
